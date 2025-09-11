b762a51d46350cefe72deb7408781ca2dbb7514e
Roberto <roberto@robertodegaetano.it>
2025-09-11 15:49:39 +0200

    tuple

--- Python/NATIVE/tuple.py
+++ Python/NATIVE/tuple.py
@@ -12,17 +12,26 @@ def swap_tuple(a, b):
     a,b = b,a
     return a,b
 
+def dict_creation(a, b):    #   creazione dizionario da 2 tuple
+    dictionary = {}
+    if len(a) == len(b):
+        for i, j in enumerate(a):
+           dictionary[j] = b[i] 
+    return dictionary
+
 def executions():
-    print(tupla)
-    print(tupla[2])
-    print(type(tupla))
-    print(tupla_2)
-    print(type(tupla_2))
-    print(tupla_3)
-    print(type(tupla_3))
-    print(tupla_4)
-    print(type(tupla_4))
-    print(tupla_4[0])       #   permesso perchè tupla
-    print(swap_tuple(tupla, tupla_4))
+    # print(tupla)
+    # print(tupla[2])
+    # print(type(tupla))
+    # print(tupla_2)
+    # print(type(tupla_2))
+    # print(tupla_3)
+    # print(type(tupla_3))
+    # print(tupla_4)
+    # print(type(tupla_4))
+    # print(tupla_4[0])       #   permesso perchè tupla
+    # print(swap_tuple(tupla, tupla_4))
+    # print(dict_creation(('a','b','c','d','e'), (1,2,3,4,5)))    # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+    pass
 
 executions()
